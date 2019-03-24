03545025001c626c6e3d9d95a4ae4625d32cd7bc
JayHChoi <jaychoi1619@gmail.com>
2019-03-24 20:05:39 +0900

    Add jest and sample test suite

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,9 +1,25 @@
-import { getFirstName } from '../src/utils/user'
+import { getFirstName, isValidPassword } from '../src/utils/user'
 
 test('Should return first name when given full name', () => {
   const firstName = getFirstName('Jay Choi')
 
-  if (firstName !== 'Jay') {
-    throw new Error('Test failed')
-  }
+  expect(firstName).toBe('Jay')
+})
+
+test('Should reject password shorter than 8 chars', () => {
+  const isValid = isValidPassword('1234567')
+
+  expect(isValid).toBe(false)
+})
+
+test('Should reject password shorter than 8 chars', () => {
+  const isValid = isValidPassword('123Password')
+
+  expect(isValid).toBe(false)
+})
+
+test('Should correctly validate a valid password', () => {
+  const isValid = isValidPassword('abcd1234!!')
+
+  expect(isValid).toBe(true)
 })
